b1d0a20fee96bb413a2a0ac8746fa522d5a98faa
山田翔太 <yamada-shouta@yamadashoutanoMacBook-Pro.local>
2020-08-12 22:45:51 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,19 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def edit
   end
 
-  def update
-  end
-
-  def destroy
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
